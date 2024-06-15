1163b62d672642c72a45deca4bc07ba4da2f8d3b
Logan Bachman <lbachman94@gmail.com>
2024-06-15 11:22:53 -0400

    added nuget packages for ef and identity framework and configured connection string

--- Trail-Tracker-App/Trail-Tracker-App/obj/Debug/net8.0/Trail-Tracker-App.AssemblyInfo.cs
+++ Trail-Tracker-App/Trail-Tracker-App/obj/Debug/net8.0/Trail-Tracker-App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trail-Tracker-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7413124a7fdb22854fe07520f61ac0b3bfc8571")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trail-Tracker-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trail-Tracker-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Trail-Tracker-App/install-packages.ps1
@@ -0,0 +1,10 @@
+<#
+These packages give you everything you need to scaffold a mysql 
+database with the entity framework. 
+#>
+
+Install-Package Microsoft.EntityFrameworkCore
+Install-Package Microsoft.EntityFrameworkCore.Design
+Install-Package Microsoft.EntityFrameworkCore.tools
+Install-Package MySql.EntityFrameworkCore
+Install-Package Microsoft.AspNetCore.Identity.EntityFrameworkCore
