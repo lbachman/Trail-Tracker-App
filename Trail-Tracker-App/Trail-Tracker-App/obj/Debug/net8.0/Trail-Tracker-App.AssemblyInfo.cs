886059a2405d432151e7c4e1b94dffd8f5b4e2f5
Logan Bachman <lbachman94@gmail.com>
2024-09-02 13:32:42 -0400

    made some changes to make look better on mobile

--- Trail-Tracker-App/Trail-Tracker-App/obj/Debug/net8.0/Trail-Tracker-App.AssemblyInfo.cs
+++ Trail-Tracker-App/Trail-Tracker-App/obj/Debug/net8.0/Trail-Tracker-App.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trail-Tracker-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c1cff06f9adec29d2917617e8aad59d20cb662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c27311b2705ea05a3882758acee337fc40fd35cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trail-Tracker-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trail-Tracker-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
